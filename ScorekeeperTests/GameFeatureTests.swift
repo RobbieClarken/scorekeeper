abbaf9ab52baff075aa6e569e476a368f351d648
RobbieClarken <robbie.clarken@gmail.com>
2026-01-28 11:22:54 +1030

    Add tests stubs

---
+++ ScorekeeperTests/GameFeatureTests.swift
@@ -0,0 +1,9 @@
+import Testing
+
+@testable import Scorekeeper
+
+struct GameFeatureTests {
+
+    @Test func basics() async throws {}
+
+}
---
+++ ScorekeeperTests/ScorekeeperTests.swift
@@ -0,0 +1,9 @@
+import Testing
+
+@testable import Scorekeeper
+
+struct ScorekeeperTests {
+
+    @Test func example() async throws {}
+
+}
